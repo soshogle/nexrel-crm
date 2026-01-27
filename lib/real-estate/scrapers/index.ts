d77e79497abd970e311e281f472de7a65fbcbb4f
Nexrel Deploy <deploy@nexrel.com>
2026-01-27 23:49:01 +0000

    Fix: Simplify scraper exports to match actual implementations

--- lib/real-estate/scrapers/index.ts
+++ lib/real-estate/scrapers/index.ts
@@ -2,39 +2,18 @@
  * Real Estate Scrapers - Exports
  */
 
-// Utils
-export { getApifyToken, getHunterApiKey, normalizePhone, normalizeAddress } from './utils';
-
-// Job Manager
+// Scraper Service (main entry point)
 export {
   createScrapingJob,
-  updateScrapingJob,
   getScrapingJobs,
-  deleteScrapingJob,
-  getJobsDueForRun,
-  runScrapingJob
-} from './scraping-job-manager';
+  updateJobStatus,
+  saveFSBOListing,
+  getFSBOListings,
+  updateFSBOStatus
+} from './scraper-service';
 
-// DuProprio (Canadian FSBO)
-export { 
-  scrapeDuProprio, 
-  checkDuProprioJobStatus,
-  saveDuProprioListings,
-  type DuProprioListing,
-  type ScrapeDuProprioConfig
-} from './duproprio';
+// Centris (Quebec)
+export { scrapeCentris, getCentrisJobStatus } from './centris';
 
-// US FSBO Sites
-export {
-  scrapeUSFSBO,
-  checkUSFSBOJobStatus,
-  type USFSBOListing,
-  type ScrapeUSFSBOConfig
-} from './us-fsbo';
-
-// Other scrapers
-export { scrapeCENTRIS, checkCENTRISJobStatus } from './centris';
-export { scrapeCRAIGSLIST, checkCRAIGSLISTJobStatus } from './craigslist';
-export { scrapeZILLOW, checkZILLOWJobStatus } from './zillow';
-export { scrapeREALTOR_CA, checkREALTOR_CAJobStatus } from './realtor-ca';
-export { scrapeREALTOR_COM, checkREALTOR_COMJobStatus } from './realtor-com';
+// Utils
+export { getApifyToken, getHunterApiKey, normalizePhone, normalizeAddress } from './utils';
